046b40b6478999d8604e2fe8baf6a61250ef3599
Samin1615088 <samin.shafkat@gmail.com>
2021-04-16 05:52:44 +0600

    service card styled

--- src/components/Home/ServiceCard/ServiceCard.js
+++ src/components/Home/ServiceCard/ServiceCard.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import './ServiceCard.scss';
 
 const ServiceCard = ({serviceData}) => {
     return (
         <div className="col">
-            <div className="card h-100">
+            <div className="card-container card h-100">
                 <img src={serviceData.serviceImg} className="card-img-top" alt="..." />
                 <div className="card-body">
-                    <h4 className="card-title">{serviceData.serviceName}</h4>
+                    <h4 className="card-title fw-bold text-uppercase">{serviceData.serviceName}</h4>
                     <p className="card-text">{serviceData.serviceDescription}</p>
                 </div>
-                <div className="card-footer d-flex justify-content-between align-items-center">
-                    <strong>$ {serviceData.price}</strong>
-                    <button className="btn btn-success text-uppercase">Buy This</button>
-                </div>
+                <footer className="card-footer d-flex justify-content-between align-items-center">
+                    <strong className="fs-3">$ {serviceData.price}</strong>
+                    <button className="btn btn-brand text-uppercase">Buy This</button>
+                </footer>
             </div>
         </div>
     );
