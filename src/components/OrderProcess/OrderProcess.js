71e0a4254e9718ac9217257707ccf956f327ed38
Samin1615088 <samin.shafkat@gmail.com>
2021-04-17 12:49:35 +0600

    react-hook-form dependency added

--- src/components/OrderProcess/OrderProcess.js
+++ src/components/OrderProcess/OrderProcess.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const OrderProcess = () => {
     return (
-        <div>
+        <div className="container">
             <h1 className="text-light">Order Process</h1>
         </div>
     );
